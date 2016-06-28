488293d6e014ceb909d26b03212b11c29b61e12b
jg <josegallegospdel@gmail.com>
2016-06-28 10:54:30 -0600

    initial

---
+++ Abracadabra/README.md
@@ -0,0 +1,4 @@
+# abracadabra
+simple regex to parse a text string activated by clicking a button
+
+
---
+++ Abracadabra/cadabra.js
@@ -0,0 +1,9 @@
+
+
+
+  function myFunction() {
+      var str = "ABRACADABRA";
+      var a = str.replace(/abracadabra/i, "ABRACADABR");
+      document.getElementById("spell").innerHTML = a;
+
+  }
